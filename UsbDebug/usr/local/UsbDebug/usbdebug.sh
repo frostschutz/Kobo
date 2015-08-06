d5432f017ac4c05857755fec41e27eecfa813a1a
Andreas Klauer <Andreas.Klauer@metamorpher.de>
2015-08-06 14:36:58 +0200

    lazy umount hook

--- UsbDebug/usr/local/UsbDebug/usbdebug.sh
+++ UsbDebug/usr/local/UsbDebug/usbdebug.sh
@@ -15,15 +15,25 @@ isbusy() {
     grep -E '^/dev/mmcblk(0p3|1p1) ' /proc/mounts || \
     grep -E '^([^ ]+ ){3}b3:0[39] ' /proc/*/maps || \
     find /proc/[0-9]*/cwd /proc/[0-9]*/fd -exec stat -tL {} + \
-    | grep -E '^([^ ]+ ){6}b30[39]'
+    | grep -E '^([^ ]+ ){6}b30[39]' ||
+    grep -E '^failure.* /mnt/(onboard|sd) ' /tmp/usbdebug-umount
 }
 
 showface() {
     if isbusy > /tmp/UsbDebug.tmp
     then
+        rm /tmp/usbdebug-umount
         mv /tmp/UsbDebug.tmp /tmp/UsbDebug.log
         pngshow /usr/local/UsbDebug/sadface.png
+        sleep 1
+        pngshow /usr/local/UsbDebug/sadface.png
+        sleep 1
+        pngshow /usr/local/UsbDebug/sadface.png
     else
+        pngshow /usr/local/UsbDebug/happyface.png
+        sleep 1
+        pngshow /usr/local/UsbDebug/happyface.png
+        sleep 1
         pngshow /usr/local/UsbDebug/happyface.png
     fi
 }
@@ -34,13 +44,13 @@ if [ "$ACTION" == "add" ]
 then
     mkdir "/tmp/UsbDebug" || exit
 
-    timeout=300 # 5 minutes
+    timeout=30 # ~ 5 minutes
 
     while [ -e /tmp/UsbDebug -a "$timeout" -gt 0 ]
     do
         timeout=$(($timeout-1))
         showface
-        sleep 1
+        sleep 10
     done
 elif [ "$ACTION" == "remove" ]
 then
