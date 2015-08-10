262e77c8a749abc06c30c7b052136c2102339f56
Andreas Klauer <Andreas.Klauer@metamorpher.de>
2015-08-10 13:02:24 +0200

    error handling

--- WebPortal/usr/local/WebPortal/httpd_preload.c
+++ WebPortal/usr/local/WebPortal/httpd_preload.c
@@ -1,3 +1,10 @@
+/*
+ * gcc -Wall -O2 -s \
+ *     -fPIC -shared -ldl \
+ *     httpd_preload.c -o httpd_preload.so
+ *
+ */
+
 #define _GNU_SOURCE
 
 #include <sys/types.h>
@@ -12,9 +19,28 @@ int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen)
     int (*c_accept)(int, struct sockaddr*, socklen_t*);
     c_accept=dlsym(RTLD_NEXT, "accept");
     char * path = getcwd(NULL, 0);
-    chdir("/");
+
+    // change cwd to rootfs to unblock /mnt/onboard
+    if(chdir("/usr/local/WebPortal") != 0)
+    {
+        exit(1);
+    }
+
+    // wait for connection
     n = c_accept(sockfd, addr, addrlen);
-    chdir(path);
+
+    // change back to /mnt/onboard to serve this request
+    while(chdir(path) != 0)
+    {
+        if(chdir("/mnt/onboard/.kobo") == 0)
+        {
+            // fatal: webportal path went awol
+            exit(1);
+        }
+
+        sleep(2);
+    }
+
     free(path);
     return n;
 }
