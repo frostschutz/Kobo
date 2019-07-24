1ce41d1e8491547ffd01eb601f0b1291d1993cba
Andreas Klauer <Andreas.Klauer@metamorpher.de>
2019-07-24 12:55:16 +0200

    MiniClock: parse input with devinputeventdump helper

--- MiniClock/usr/local/MiniClock/miniclock.sh
+++ MiniClock/usr/local/MiniClock/miniclock.sh
@@ -57,9 +57,13 @@ load_config() {
     touch -r "$CONFIGFILE" /tmp/MiniClock # remember timestamp
 
     uninstall_check
+    cfg_debug=$(config debug '0')
 
     cfg_touchscreen=$(config touchscreen '1')
     cfg_button=$(config button '0')
+
+    cfg_whitelist=$(config whitelist 'ABS:MT_POSITION_X ABS:MT_POSITION_Y KEY:F23 KEY:F24 KEY:POWER')
+    cfg_cooldown=$(config cooldown '3 30')
 
     cfg_format=$(config format '%a %b %d %H:%M')
     cfg_offset_x=$(config offset_x '0')
@@ -149,6 +153,11 @@ load_config() {
         ;;
     esac
 
+    case "$cfg_format $cfg_truetype_format" in
+        *{debug}*) cfg_causality=1 ;;
+        *)         cfg_causality=0 ;;
+    esac
+
     # patch handling
     if [ ! -e /tmp/MiniClock/patch -a "$cfg_button" == "1" ]
     then
@@ -161,6 +170,18 @@ load_config() {
             touch /tmp/MiniClock/reboot
         fi
     fi
+
+    # whitelist filtering (string to number)
+    set -- $cfg_whitelist
+    cfg_whitelist=""
+    for item in $@
+    do
+        set -- ${item//:/ }
+        [ $# != 2 ] && continue
+        set -- $(input_event_str2int $1 $2)
+        [ $# != 2 ] && continue
+        cfg_whitelist="$cfg_whitelist $1:$2"
+    done
 }
 
 # string replace str a b
@@ -172,7 +193,7 @@ str_replace() {
     echo "$pre$3$post"
 }
 
-# shenaniganize date
+# shenaniganize date (runs in a subshell)
 shenaniganize_date() {
     local datestr=$(date "$@")
     local pre post number
@@ -185,7 +206,7 @@ shenaniganize_date() {
                 battery=$(cat "$cfg_battery_source")
                 if [ $? -eq 0 -a "$battery" -ge "$cfg_battery_min" -a "$battery" -le "$cfg_battery_max" ]
                 then
-                    battery="$battery""%%"
+                    battery="${battery}%"
                 else
                     battery=""
                 fi
@@ -205,7 +226,7 @@ shenaniganize_date() {
             ;;
             *{debug}*)
                 read uptime runtime < /proc/uptime
-                datestr=$(str_replace "$datestr" "{debug}" "|$causality:$uptime|")
+                datestr=$(str_replace "$datestr" "{debug}" "[${causality} @ ${uptime}]")
             ;;
             *)
                 echo "$datestr"
@@ -237,8 +258,6 @@ nightmode_check() {
 }
 
 update() {
-    mkdir /tmp/MiniClock/update || return
-
     sleep 0.1
 
     ( # subshell
@@ -259,7 +278,7 @@ update() {
               $nightmode \
               "$(my_tt_date +"$cfg_truetype_format")"
 
-        [ $? -eq 0 ] && rmdir /tmp/MiniClock/update && exit # return
+        [ $? -eq 0 ] && exit # return
     fi
 
     # fbink with builtin font
@@ -271,17 +290,107 @@ update() {
     ) # subshell end / unblock
 
     [ -e /tmp/MiniClock/reboot ] && fbink "MiniClock: Please Reboot for Button Patch"
-
-    rmdir /tmp/MiniClock/update
 }
 
-# expect X seconds for touch
-# return 0 if touched
-# return 1 if not touched
-timeout_touch() {
-    local touched="not"
-    read -t "$1" touched < "$2"
-    [ "$touched" != "not" ]
+# --- Input Event Helpers: ---
+
+EV="SYN KEY REL ABS MSC SW 0x6 0x7 0x8 0x9 0xa 0xb 0xc 0xd 0xe 0xf 0x10 LED SND 0x13 REP FF PWR FF_STATUS"
+EV_SYN="REPORT CONFIG MT_REPORT DROPPED"
+EV_KEY="RESERVED ESC 1 2 3 4 5 6 7 8 9 0 MINUS EQUAL BACKSPACE TAB Q W E R T Y U I O P LEFTBRACE RIGHTBRACE ENTER LEFTCTRL A S D F G H J K L SEMICOLON APOSTROPHE GRAVE LEFTSHIFT BACKSLASH Z X C V B N M COMMA DOT SLASH RIGHTSHIFT KPASTERISK LEFTALT SPACE CAPSLOCK F1 F2 F3 F4 F5 F6 F7 F8 F9 F10 NUMLOCK SCROLLLOCK KP7 KP8 KP9 KPMINUS KP4 KP5 KP6 KPPLUS KP1 KP2 KP3 KP0 KPDOT 0x54 ZENKAKUHANKAKU 102ND F11 F12 RO KATAKANA HIRAGANA HENKAN KATAKANAHIRAGANA MUHENKAN KPJPCOMMA KPENTER RIGHTCTRL KPSLASH SYSRQ RIGHTALT LINEFEED HOME UP PAGEUP LEFT RIGHT END DOWN PAGEDOWN INSERT DELETE MACRO MUTE VOLUMEDOWN VOLUMEUP POWER KPEQUAL KPPLUSMINUS PAUSE SCALE KPCOMMA HANGEUL HANJA YEN LEFTMETA RIGHTMETA COMPOSE STOP AGAIN PROPS UNDO FRONT COPY OPEN PASTE FIND CUT HELP MENU CALC SETUP SLEEP WAKEUP FILE SENDFILE DELETEFILE XFER PROG1 PROG2 WWW MSDOS COFFEE ROTATE_DISPLAY CYCLEWINDOWS MAIL BOOKMARKS COMPUTER BACK FORWARD CLOSECD EJECTCD EJECTCLOSECD NEXTSONG PLAYPAUSE PREVIOUSSONG STOPCD RECORD REWIND PHONE ISO CONFIG HOMEPAGE REFRESH EXIT MOVE EDIT SCROLLUP SCROLLDOWN KPLEFTPAREN KPRIGHTPAREN NEW REDO F13 F14 F15 F16 F17 F18 F19 F20 F21 F22 F23 F24 0xc3 0xc4 0xc5 0xc6 0xc7 PLAYCD PAUSECD PROG3 PROG4 DASHBOARD SUSPEND CLOSE PLAY FASTFORWARD BASSBOOST PRINT HP CAMERA SOUND QUESTION EMAIL CHAT SEARCH CONNECT FINANCE SPORT SHOP ALTERASE CANCEL BRIGHTNESSDOWN BRIGHTNESSUP MEDIA SWITCHVIDEOMODE KBDILLUMTOGGLE KBDILLUMDOWN KBDILLUMUP SEND REPLY FORWARDMAIL SAVE DOCUMENTS BATTERY BLUETOOTH WLAN UWB UNKNOWN VIDEO_NEXT VIDEO_PREV BRIGHTNESS_CYCLE BRIGHTNESS_AUTO DISPLAY_OFF WWAN RFKILL MICMUTE 0xf9 0xfa 0xfb 0xfc 0xfd 0xfe 0xff BTN_0 BTN_1 BTN_2 BTN_3 BTN_4 BTN_5 BTN_6 BTN_7 BTN_8 BTN_9 0x10a 0x10b 0x10c 0x10d 0x10e 0x10f BTN_LEFT BTN_RIGHT BTN_MIDDLE BTN_SIDE BTN_EXTRA BTN_FORWARD BTN_BACK BTN_TASK 0x118 0x119 0x11a 0x11b 0x11c 0x11d 0x11e 0x11f BTN_TRIGGER BTN_THUMB BTN_THUMB2 BTN_TOP BTN_TOP2 BTN_PINKIE BTN_BASE BTN_BASE2 BTN_BASE3 BTN_BASE4 0x12a 0x12b 0x12c 0x12d 0x12e 0x12f BTN_SOUTH BTN_EAST BTN_C BTN_NORTH BTN_WEST BTN_Z BTN_TL BTN_TR BTN_TL2 BTN_TR2 0x13a 0x13b 0x13c 0x13d 0x13e 0x13f BTN_TOOL_PEN BTN_TOOL_RUBBER BTN_TOOL_BRUSH BTN_TOOL_PENCIL BTN_TOOL_AIRBRUSH BTN_TOOL_FINGER BTN_TOOL_MOUSE BTN_TOOL_LENS BTN_TOOL_QUINTTAP BTN_STYLUS3 0x14a 0x14b 0x14c 0x14d 0x14e 0x14f BTN_GEAR_DOWN BTN_GEAR_UP 0x152 0x153 0x154 0x155 0x156 0x157 0x158 0x159 0x15a 0x15b 0x15c 0x15d 0x15e 0x15f OK SELECT GOTO CLEAR POWER2 OPTION INFO TIME VENDOR ARCHIVE 0x16a 0x16b 0x16c 0x16d 0x16e 0x16f LANGUAGE TITLE SUBTITLE ANGLE FULL_SCREEN MODE KEYBOARD ASPECT_RATIO PC TV 0x17a 0x17b 0x17c 0x17d 0x17e 0x17f TAPE RADIO TUNER PLAYER TEXT DVD AUX MP3 AUDIO VIDEO 0x18a 0x18b 0x18c 0x18d 0x18e 0x18f YELLOW BLUE CHANNELUP CHANNELDOWN FIRST LAST AB NEXT RESTART SLOW 0x19a 0x19b 0x19c 0x19d 0x19e 0x19f 0x1a0 0x1a1 0x1a2 0x1a3 0x1a4 0x1a5 0x1a6 0x1a7 0x1a8 0x1a9 0x1aa 0x1ab 0x1ac 0x1ad 0x1ae 0x1af 0x1b0 0x1b1 0x1b2 0x1b3 0x1b4 0x1b5 0x1b6 0x1b7 0x1b8 0x1b9 0x1ba 0x1bb 0x1bc 0x1bd 0x1be 0x1bf 0x1c0 0x1c1 0x1c2 0x1c3 0x1c4 0x1c5 0x1c6 0x1c7 0x1c8 0x1c9 0x1ca 0x1cb 0x1cc 0x1cd 0x1ce 0x1cf 0x1d0 0x1d1 0x1d2 0x1d3 0x1d4 0x1d5 0x1d6 0x1d7 0x1d8 0x1d9 0x1da 0x1db 0x1dc 0x1dd 0x1de 0x1df 0x1e0 0x1e1 0x1e2 0x1e3 0x1e4 0x1e5 0x1e6 0x1e7 0x1e8 0x1e9 0x1ea 0x1eb 0x1ec 0x1ed 0x1ee 0x1ef 0x1f0 0x1f1 0x1f2 0x1f3 0x1f4 0x1f5 0x1f6 0x1f7 0x1f8 0x1f9 0x1fa 0x1fb 0x1fc 0x1fd 0x1fe 0x1ff NUMERIC_0 NUMERIC_1 NUMERIC_2 NUMERIC_3 NUMERIC_4 NUMERIC_5 NUMERIC_6 NUMERIC_7 NUMERIC_8 NUMERIC_9 0x20a 0x20b 0x20c 0x20d 0x20e 0x20f CAMERA_FOCUS WPS_BUTTON TOUCHPAD_TOGGLE TOUCHPAD_ON TOUCHPAD_OFF CAMERA_ZOOMIN CAMERA_ZOOMOUT CAMERA_UP CAMERA_DOWN CAMERA_LEFT 0x21a 0x21b 0x21c 0x21d 0x21e 0x21f BTN_DPAD_UP BTN_DPAD_DOWN BTN_DPAD_LEFT BTN_DPAD_RIGHT 0x224 0x225 0x226 0x227 0x228 0x229 0x22a 0x22b 0x22c 0x22d 0x22e 0x22f ALS_TOGGLE ROTATE_LOCK_TOGGLE 0x232 0x233 0x234 0x235 0x236 0x237 0x238 0x239 0x23a 0x23b 0x23c 0x23d 0x23e 0x23f BUTTONCONFIG TASKMANAGER JOURNAL CONTROLPANEL APPSELECT SCREENSAVER VOICECOMMAND ASSISTANT 0x248 0x249 0x24a 0x24b 0x24c 0x24d 0x24e 0x24f BRIGHTNESS_MIN BRIGHTNESS_MAX 0x252 0x253 0x254 0x255 0x256 0x257 0x258 0x259 0x25a 0x25b 0x25c 0x25d 0x25e 0x25f KBDINPUTASSIST_PREV KBDINPUTASSIST_NEXT KBDINPUTASSIST_PREVGROUP KBDINPUTASSIST_NEXTGROUP KBDINPUTASSIST_ACCEPT KBDINPUTASSIST_CANCEL RIGHT_UP RIGHT_DOWN LEFT_UP LEFT_DOWN 0x26a 0x26b 0x26c 0x26d 0x26e 0x26f NEXT_FAVORITE STOP_RECORD PAUSE_RECORD VOD UNMUTE FASTREVERSE SLOWREVERSE DATA ONSCREEN_KEYBOARD"
+EV_REL="X Y Z RX RY RZ HWHEEL DIAL WHEEL MISC"
+EV_ABS="X Y Z RX RY RZ THROTTLE RUDDER WHEEL GAS 0xa 0xb 0xc 0xd 0xe 0xf HAT0X HAT0Y HAT1X HAT1Y HAT2X HAT2Y HAT3X HAT3Y PRESSURE DISTANCE 0x1a 0x1b 0x1c 0x1d 0x1e 0x1f VOLUME 0x21 0x22 0x23 0x24 0x25 0x26 0x27 MISC 0x29 0x2a 0x2b 0x2c 0x2d 0x2e 0x2f MT_TOUCH_MAJOR MT_TOUCH_MINOR MT_WIDTH_MAJOR MT_WIDTH_MINOR MT_ORIENTATION MT_POSITION_X MT_POSITION_Y MT_TOOL_TYPE MT_BLOB_ID MT_TRACKING_ID"
+EV_MSC="SERIAL PULSELED GESTURE RAW SCAN TIMESTAMP 0x6 MAX"
+EV_SW="LID TABLET_MODE HEADPHONE_INSERT RFKILL_ALL MICROPHONE_INSERT DOCK LINEOUT_INSERT JACK_PHYSICAL_INSERT VIDEOOUT_INSERT CAMERA_LENS_COVER"
+EV_LED="NUML CAPSL SCROLLL COMPOSE KANA SLEEP SUSPEND MUTE MISC MAIL"
+EV_SND="CLICK BELL TONE 0x3 0x4 0x5 0x6 MAX"
+EV_REP="DELAY MAX"
+
+# convert event string to number
+input_event_str2int() {
+    local type="$1"
+    local code="$2"
+
+    case $type in
+        SYN) set -- $EV_SYN ;;
+        KEY) set -- $EV_KEY ;;
+        REL) set -- $EV_REL ;;
+        ABS) set -- $EV_ABS ;;
+        MSC) set -- $EV_MSC ;;
+        SW)  set -- $EV_SW  ;;
+        LED) set -- $EV_LED ;;
+        SND) set -- $EV_SND ;;
+        REP) set -- $EV_REP ;;
+        *)   set --         ;;
+    esac
+
+    code=$(printf " %s \n" $@  | grep -n -F " $code ")
+    [ $? -eq 0 ] && code=$((${code%%:*}-1))
+    type=$(printf " %s \n" $EV | grep -n -F " $type ")
+    [ $? -eq 0 ] && type=$((${type%%:*}-1))
+
+    echo $type $code
+}
+
+# convert event number to string
+input_event_int2str() {
+    local type="$1"
+    local code="$2"
+
+    set -- $EV
+    [ $# -gt $type ] && shift $type || set --
+    type=${1:-$type}
+
+    case $type in
+        SYN) set -- $EV_SYN ;;
+        KEY) set -- $EV_KEY ;;
+        REL) set -- $EV_REL ;;
+        ABS) set -- $EV_ABS ;;
+        MSC) set -- $EV_MSC ;;
+        SW)  set -- $EV_SW  ;;
+        LED) set -- $EV_LED ;;
+        SND) set -- $EV_SND ;;
+        REP) set -- $EV_REP ;;
+        *)   set --         ;;
+    esac
+
+    [ $# -gt $code ] && shift $code || set --
+    code=${1:-$code}
+
+    echo $type $code
+}
+
+check_event() {
+    [ "$cfg_debug" = 1 ] && debug_event "$@"
+
+    while [ $# -ge 5 ]
+    do
+        for item in $cfg_whitelist
+        do
+            if [ "$item" = "$3:$4" ]
+            then
+                # successful
+                [ "$cfg_causality" = 1 ] && causality="$(input_event_int2str $3 $4 | tr ' ' ':') $5"
+                return 0
+            fi
+        done
+        shift 5
+    done
+
+    # unsuccessful
+    return 1
+}
+
+debug_event() {
+    eventstr="MiniClock debug event:"
+
+    while [ $# -ge 5 ]
+    do
+        eventstr="$eventstr"$'\n'"[$1 $(input_event_int2str $3 $4 | tr ' ' ':') $5]"
+        shift 5
+    done
+
+    sleep 3
+    fbink -x 2 -y 2 "$eventstr"
 }
 
 # --- Main: ---
@@ -289,61 +398,47 @@ timeout_touch() {
 main() {
     local i=0
     local x=0
+    local negative=0
 
     udev_workarounds
     wait_for_nickel
-    mkfifo /tmp/MiniClock/loop
 
-    while [ -p /tmp/MiniClock/loop ]
+    while sleep 1
     do
         load_config
         nightmode_check
-        causality="freq"
 
-        if [ "$cfg_touchscreen" = 1 ]
+        if check_event $(devinputeventdump /dev/input/event1 /dev/input/event0)
         then
-        (
-           mkdir /tmp/MiniClock/touchscreen || exit
-           timeout_touch $((1 + $cfg_update - ( ($(date +%s)+$cfg_delta) % $cfg_update))) /dev/input/event1 && causality=touch
-
+           # event coming in hot
            for i in $cfg_delay
            do
                sleep $i
                update
            done
-           rmdir /tmp/MiniClock/touchscreen
-           echo next > /tmp/MiniClock/loop
-       ) &
-       fi
+           negative=0
+        else
+           # unknown event, cold
+           negative=$(($negative+1))
 
-       if [ "$cfg_button" = 1 ]
-       then
-       (
-           mkdir /tmp/MiniClock/button || exit
-           timeout_touch $((1 + $cfg_update - ( ($(date +%s)+$cfg_delta) % $cfg_update))) /dev/input/event0 && causality=butt
+           # getting cold events in a row? sleep a while.
+           if [ "$negative" -ge "${cfg_cooldown% *}" ]
+           then
+               if [ "$cfg_causality" = 1 ]
+               then
+                   # update only to display cooldown {debug}
+                   causality="cooldown $cfg_cooldown"
+                   for i in $cfg_delay
+                   do
+                       sleep $i
+                       update
+                   done
+               fi
 
-           for i in $cfg_delay
-           do
-               sleep $i
-               update
-           done
-           rmdir /tmp/MiniClock/button
-           echo next > /tmp/MiniClock/loop
-       ) &
-       fi
-
-       if [ "$cfg_touchscreen" = 0 -a "$cfg_button" = 0 ]
-       then
-       (
-           mkdir /tmp/MiniClock/frequency || exit
-           sleep $(( ($(date +%s)+$cfg_delta) % $cfg_update ))
-           update
-           rmdir /tmp/MiniClock/frequency
-           echo next > /tmp/MiniClock/loop
-       ) &
-       fi
-
-       read -t $cfg_update next < /tmp/MiniClock/loop
+               sleep "${cfg_cooldown#* }"
+               negative=0
+           fi
+        fi
     done
 }
 
