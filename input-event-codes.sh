3c6991e5f140cee47a2fe93ab73a52f0041d5c6a
Andreas Klauer <Andreas.Klauer@metamorpher.de>
2019-07-25 09:49:07 +0200

    fix missing BTN_ and 0xa-f events

--- input-event-codes.sh
+++ input-event-codes.sh
@@ -6,26 +6,32 @@
 
 parse_prefix() {
     prev=-1
-    grep -F "#define ${1}_" /usr/include/linux/input-event-codes.h |
+    sed -e 's@ BTN_@ KEY_BTN_@g' /usr/include/linux/input-event-codes.h |
+    grep -F "#define ${1}_" |
     while read define name value comment
     do
-        [[ "$value" =~ ^[0-9x]+$ ]] || continue
+        [[ "$value" =~ ^[0-9xa-fA-F]+$ ]] || continue
         echo $(($value)) "$name"
-    done | sort -n |
+    done |
     while read value name
     do
-        [ $prev = $value ] && continue
+        [ $prev -gt $value ] && echo "$name appeared out of order: $prev > $value ..." >&2 && continue
+        [ $prev = $value ] && echo -n "/${name#*_}" && continue
+        printf "\n"
         prev=$(($prev+1))
         while [ $prev -lt $value ]
         do
             printf "0x%x\n" "${prev}"
             prev=$(($prev+1))
         done
-        echo "${name#*_}"
-    done
+        echo -n "${name#*_}"
+    done |
+    sed -r -e 's@[^\s]*/@@g'
 }
 
-echo EV='"'$(parse_prefix EV)'"'
+set -- $(parse_prefix EV)
+
+echo EV='"'$@'"'
 
 for ev in $(parse_prefix EV)
 do
